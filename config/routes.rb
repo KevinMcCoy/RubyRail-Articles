98628bce708427a1c62b9c5541074d59b28884d1
Kevin McCoy <KevinMcCoy@earthlink.net>
2014-08-15 05:24:09 -0400

    Landing page to article

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  get 'main/index'
-  root 'main#index'
+  get 'articles/index'
+  root 'articles#index'
 
 
   # Example of regular route:
