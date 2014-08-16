a5a13c5985c394ba9167d455d11294ac35592381
Kevin McCoy <KevinMcCoy@earthlink.net>
2014-08-16 17:46:30 -0400

    Section 7 and 8 is completed
    7 Refactoring
    8 Deleting Comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
-	has_many :comments
+
+	#dependent: destory will destory comment if releated article is deleted
+	has_many :comments, dependent: :destroy
 
 	# In articles_controller -> def create
 	validates :title, presence: true, length: { minimum: 5 }
